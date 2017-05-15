a88c58fe465a6ecfcd43a57bc4bc58459c0ce032
sibukixxx <sibukixxx@gmail.com>
2017-05-15 15:27:16 +0900

    はじめての Django アプリ作成、その5

--- assam/polls/views.py
+++ assam/polls/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -14,9 +15,7 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
-
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
